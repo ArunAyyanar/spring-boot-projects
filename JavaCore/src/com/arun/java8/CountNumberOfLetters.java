03d6221f283e10837e357c312de936bf50fcf9d0
Arun Ayyanar <arunctech89@gmail.com>
2024-08-20 22:31:46 -0400

    eachLettercount

--- JavaCore/src/com/arun/java8/CountNumberOfLetters.java
+++ JavaCore/src/com/arun/java8/CountNumberOfLetters.java
@@ -1,17 +1,27 @@
 package com.arun.java8;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CountNumberOfLetters {
 
 	public static void main(String[] args) {
 
-		
 		String message = "welcome to java world";
+
+		// traditional way
+
+		Map<Character, Integer> countMap = new HashMap<>();
+		for (char ch : message.toCharArray()) {
+			if (countMap.containsKey(ch)) {
+				countMap.put(ch, countMap.get(ch) + 1);
+			} else {
+				countMap.put(ch, 1);
+			}
+		}
 		
-		
-		
-		
-		
-		
+		System.out.println(countMap);
+
 	}
 
 }
